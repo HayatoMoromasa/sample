7ef9602bb34c8d9a43cb2bc7b085b203ff743cbe
HayatoMoromasa <bboy.hayato1tribal@gmail.com>
2020-11-07 15:34:54 +0900

    liquid-image-sample

---
+++ liquidImages/app.js
@@ -0,0 +1,10 @@
+new hoverEffect({
+    parent: document.querySelector('.liquid-images'),
+    intensity: 0.1,
+    image1: './freeze01.jpg',
+    image2: './freeze02.jpg',
+    angle: Math.PI / 8,
+    speedIn: 1.6,
+    speedOut: 1.2,
+    displacementImage: './freeze02.jpg'
+});
